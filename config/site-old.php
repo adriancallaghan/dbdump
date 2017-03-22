24281db1fc3a4c11c9f0c5e47ab3fedf5d9e9265
Adrian Callaghan <admin@adriancallaghan.co.uk>
2017-03-22 11:12:24 +0000

    finto

--- config/site-old.php
+++
@@ -1,5 +0,0 @@
-<?php
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'neolegal_s00kwa6');
-define('DB_PASSWORD', 'T454Jx]#0v2}');
-define('DB_DATABASE', 'neolegal_v00cha4sW');
--- config/site.php
+++
@@ -1,5 +0,0 @@
-<?php
-define('DB_SERVER', 'mysql1.clusterdb.net');
-define('DB_USERNAME', 's00kw-gam-u-069089');
-define('DB_PASSWORD', 'T454Jx]#0v2}');
-define('DB_DATABASE', 's00kw-gam-u-069089');
